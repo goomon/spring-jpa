aadf7307e133334a5e24659e856e0659139383a9
goomon <jaegoo.jho@gmail.com>
2023-12-21 23:49:56 +0900

    first commit

---
+++ src/test/kotlin/com/github/goomon/jpa/JpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.goomon.jpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
