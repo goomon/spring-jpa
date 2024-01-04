560d480811ed4f741698d10195bbe1aab652f9f0
goomon <jaegoo.jho@gmail.com>
2024-01-04 22:13:26 +0900

    파일 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
 	implementation("com.vladmihalcea.flexy-pool:flexy-micrometer-metrics:2.2.3")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.kotest:kotest-assertions-core:5.8.0")
+	testImplementation("io.kotest:kotest-property:5.8.0")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS post;
-CREATE TABLE post (
-    id bigint auto_increment primary key,
-    title varchar(255) not null,
-    creator varchar(255) not null,
-    created_at datetime default CURRENT_TIMESTAMP not null
-);
